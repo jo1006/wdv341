787047bb1eb694935a662b5fd26c12eeae885070
jo1006 <35949195+jo1006@users.noreply.github.com>
2020-01-20 19:52:51 -0600

    adding testGit.php - first file to repo

---
+++ testGit.php
@@ -0,0 +1,21 @@
+<?php 
+
+	$firstName = "JoAnn"; //datatype:string  scope:global
+	$lastName = "Weaver"; //datatype:string  scope:global
+	
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>testGit.php to/from GitHub</title>
+<!-- JoAnn Weaver	
+	 01/20/2020
+-->
+	 
+</head>
+<body>
+	<h2><?php echo $firstName." ".$lastName ?></h2> 
+
+<body>
+</html>
