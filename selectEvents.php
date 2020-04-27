490163bd31890d6215d47c8e66ad08f914dc641c
jo1006 <35949195+jo1006@users.noreply.github.com>
2020-04-27 18:07:29 -0500

    Unit 9A - Select Events

---
+++ selectEvents.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>EventID</th><th>EventName</th><th>EventDescription</th><th>EventPresenter</th><th>EventDate</th><th>EventTime</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+require 'dbConnect.php'; //access and run this external file 
+
+try {
+	
+	$stmt = $conn->prepare("SELECT event_id, event_name, event_description, event_presenter, event_date, event_time FROM wdv341_event");
+	$stmt->execute();
+	
+	$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+	foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+
+
+
+} 
+catch(PDOException $e) {
+	echo "Error: " . $e->getMessage();
+}
+
+echo "</table>";
+$conn = null;
+
+?>
+</body>
+</html>
